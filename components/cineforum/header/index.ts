c3036da16ca35710d7314c5c6087204774917a73
Giorgio Pagliaro <giopag27@gmail.com>
2026-02-23 12:02:36 +0100

    removed cineforum header

--- components/cineforum/header/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CineforumHeader } from "./CineforumHeader";
--- pages/cineforum/[cineforumId]/index.tsx
+++ pages/cineforum/[cineforumId]/index.tsx
@@ -3,8 +3,6 @@ import type { GetServerSideProps } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import prisma from "@/lib/prisma";
-
-import { CineforumHeader } from "@/components/cineforum/header";
 import { CreateProposal } from "@/components/cineforum/proposal/create";
 import { OpenProposal } from "@/components/cineforum/proposal/open";
 import { ClosedProposal } from "@/components/cineforum/proposal/closed";
@@ -100,11 +98,6 @@ export default function CineforumHome({
   return (
     <Layout>
       <div>
-        <CineforumHeader
-          title={cineforumName}
-          subtitle="Vote, see results, or create a new proposal — all in one place."
-        />
-
         {showClosedProposal && <ClosedProposal last={last} />}
 
         {showCreateProposal && <CreateProposal cineforumId={cineforumId} />}
